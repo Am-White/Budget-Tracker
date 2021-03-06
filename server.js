8a97fd448a563645468b5eab6d76cbd335684f3a
littlefire <aybwhite@gmail.com>
2021-03-05 19:46:46 -0800

    edits to the server to set up for mongodb

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+//Mongoose connection with mongodb through heroku
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex:true,
   useFindAndModify: false
 });
 
