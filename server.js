1542efa624a50c058263e78145e54f5d2ee1a5cd
littlefire <aybwhite@gmail.com>
2021-03-06 09:51:39 -0800

    still trying to fix heroku issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 //Mongoose connection with mongodb through heroku
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/oflineDb", {
+  process.env.MONGODB_URI || "mongodb://localhost/offlineDb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex:true,
