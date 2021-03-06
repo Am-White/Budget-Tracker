237aaf3d9e3b0284327f5ffd4a74a714cd0a3e51
littlefire <aybwhite@gmail.com>
2021-03-05 21:21:08 -0800

    finsihed offlineDb?

--- public/offlineDb.js
+++ public/offlineDb.js
@@ -27,3 +27,31 @@ function saveRecord(record) {
     const pendingStore = transaction.objectStore("pending");
     pendingStore.add(record);
 }
+// checkDatabase - when back online post saved transactions and clear indexedDB
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    const getAll = pendingStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*', 'Content-Type': 'application/json',
+
+                },
+            })
+            .then((response) => response.json())
+            .then(() => {
+                // open transaction on pending db, access pending object store and clear all items 
+                const transaction = db.transaction(["pending"], "readwrite");
+                const objectStore = transaction.objectStore("pending");
+                objectStore.clear();
+            });
+        }
+    };
+}
+// listener
+window.addEventListener('online', checkDatabase);
